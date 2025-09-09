40b46a033186c061f2f43d6995cd0e32b77be1e3
alexandermori0917 <alexandermori1218@gmail.com>
2025-09-09 19:28:45 +0300

    Finished the frontend

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,9 @@
 import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
 import { provideHttpClient } from "@angular/common/http";
+import { provideAnimations } from "@angular/platform-browser/animations";
 import { MessageService } from "primeng/api";
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient(), MessageService],
+  providers: [provideHttpClient(), provideAnimations(), MessageService],
 }).catch((err) => console.error(err));
